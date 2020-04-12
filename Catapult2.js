b12902fef1ef52eeea2d046cb12fc2309b54f1b0
Asaawari <61871556+Asaawari@users.noreply.github.com>
2020-04-12 16:15:42 +0530

    Add files via upload
    ss1

---
+++ Catapult2.js
@@ -0,0 +1,7 @@
+class C2 extends BaseClass{
+    constructor(x,y,height,angle){
+      super(x,y,20,height,angle);
+      this.image = loadImage("sprites/sling2.png");
+      Matter.Body.setAngle(this.body, angle);
+    }
+  }
